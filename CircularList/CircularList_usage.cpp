31e1b06b53b542ce357e6e73c2c0a7bf644da61a
GuoJTim <bnpiglike@gmail.com>
2021-12-25 23:52:23 +0800

    CircularListFull usage 更新
    支援 自訂義的 compare function 用於 insert sort 在排序好的序列中 插入某值

--- CircularList/CircularList_usage.cpp
+++ CircularList/CircularList_usage.cpp
@@ -1,8 +1,12 @@
-#include "CircularList.h" // a circular list with last node
-//#include "CircularListFull.h" // a circular list with head and tail node and sentinel
+//#include "CircularList.h" // a circular list with last node
+#include "CircularListFull.h" // a circular list with head and tail node and sentinel
 #include "../Chain/Chain.h"
 #include <iostream>
 using namespace std;
+
+bool comp(int a , int b){
+	return a < b;
+}
 
 int main(){
 	
@@ -20,6 +24,8 @@ int main(){
 	list.Delete(1); // remove last node
 	list.print();
 	
+	
+	
 	{
 		CircularList<int> testAVlist;// when destructor list will replace to av list
 		testAVlist.InsertFront(5);
@@ -30,4 +36,34 @@ int main(){
 	Chain<int> chain(CircularList<int>::GetAVlist());
 	cout << "get AV list:"; chain.Print();
 	
+	list.InsertFront(5);
+	list.InsertFront(4);
+	list.InsertFront(3);
+	cout << "org list:";list.print();
+	CircularList<int> copyClass(list);
+	cout << "copy list:";copyClass.print();
+	copyClass.Delete(0);
+	cout << "copy list (delete 0):";copyClass.print();
+	cout << "org list:";list.print();
+	
+	CircularList<int> empty;
+	CircularList<int> copyEmpty(empty);
+	copyEmpty.InsertFront(0);
+	copyEmpty.print();
+	
+	
+#ifdef CircularListFull_H
+	// test for custom compare function usage
+	CircularList<int> org_insert_sort;
+	org_insert_sort.InsertSort(new ChainNode<int>(1));
+	org_insert_sort.InsertSort(new ChainNode<int>(4));
+	org_insert_sort.InsertSort(new ChainNode<int>(2));
+	org_insert_sort.print();
+	
+	CircularList<int> custom_insert_sort;
+	custom_insert_sort.InsertSort(new ChainNode<int>(1),&comp);
+	custom_insert_sort.InsertSort(new ChainNode<int>(4),&comp);
+	custom_insert_sort.InsertSort(new ChainNode<int>(2),&comp);
+	custom_insert_sort.print();
+#endif 
 }
