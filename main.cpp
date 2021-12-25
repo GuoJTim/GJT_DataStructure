cbf2031a132b816dd6345fd8c79c33112cf56fe8
GuoJTim <bnpiglike@gmail.com>
2021-12-25 23:53:13 +0800

    update

--- main.cpp
+++ main.cpp
@@ -1,12 +1,35 @@
 #include <iostream>
-#include "Polynomial/Polynomial.h"
+#include "CircularList/CircularListFull.h"
+#include "ChainIterator/ChainIterator.h"
 using namespace std;
 
+class student{
+	public:
+		int age,id;
+		student(int age,int id){
+			this->age = age;
+			this->id = id;
+		}
+		student(){
+			
+		}
+};
+
+bool comp(student A,student B){
+	if (A.age == B.age){
+		return A.id > B.id;
+	}else return A.age > B.age;
+}
+
 int main(){
-	Polynomial a,b;
-	cin >> a;
-	cin >> b;
-	Polynomial c = a+b;
-	cout << "finished" << endl;
-	cout << c;
+	CircularList<student> studentList;
+	studentList.InsertSort(new ChainNode<student>(student(5,10)),&comp);
+	studentList.InsertSort(new ChainNode<student>(student(5,17)),&comp);
+	studentList.InsertSort(new ChainNode<student>(student(5,12)),&comp);
+	studentList.InsertSort(new ChainNode<student>(student(5,9)),&comp);
+	studentList.InsertSort(new ChainNode<student>(student(5,11)),&comp);
+	studentList.InsertSort(new ChainNode<student>(student(2,11)),&comp);
+	for(CircularList<student>::ChainIterator it = studentList.begin(); it != studentList.end();it++){
+		cout << it->age << " " << it->id << endl;
+	}
 }    
